344744a1a04297ba8fa421b58f82b9ccfd8a951a
kwleiker <kwleiker@gmail.com>
2019-09-18 23:47:36 -0500

    Add files via upload

---
+++ EmployeeSQL/query1.sql
@@ -0,0 +1,12 @@
+SELECT
+    emp_no, 
+	birth_date, 
+	first_name, 
+	last_name, 
+	gender
+FROM
+   employees 
+
+/** polite form above, standard form below: 
+SELECT emp_no, birth_date, first_name, last_name, gender, hire_date FROM employees 
+	**/
